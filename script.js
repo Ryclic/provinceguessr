e8f4e3b48e227b2df2f199d754feb6f13d145227
Ryclic <ryanchen175@gmail.com>
2023-07-17 10:51:33 -0700

    barebones map and province outlines

---
+++ script.js
@@ -0,0 +1,77 @@
+import {provincesData} from "./provinces.js";
+
+// Default outline for all provinces
+function style(feature) {
+    return {
+        fillColor: '#ef7678',
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7
+    };
+}
+
+// Outline on mouseover
+function highlightFeature(e) {
+    var layer = e.target;
+
+    layer.setStyle({
+        weight: 5,
+        color: '#7e92ed',
+        dashArray: '',
+        fillOpacity: 0.7
+    });
+
+    layer.bringToFront();
+    info.update(layer.feature.properties);
+}
+
+function resetHighlight(e) {
+    geojson.resetStyle(e.target);
+    info.update();
+}
+
+function zoomToFeature(e) {
+    map.fitBounds(e.target.getBounds());
+}
+
+// Setup event listeners
+function featureEffects(feature, layer){
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        click: zoomToFeature
+    });
+}
+
+// Render the 2D map
+var map = L.map('map').fitWorld();
+L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© CartoDB'
+}).addTo(map);
+
+// Imported province data setup with Leaflet, setup map styles
+var geojson;
+geojson = L.geoJson(provincesData, {
+    style: style,
+    onEachFeature: featureEffects
+}).addTo(map);
+
+// Create a key for provinces
+var info = L.control();
+
+info.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'info');
+    this.update();
+    return this._div;
+};
+
+info.update = function (props) {
+    this._div.innerHTML = '<h3>Current Province:</h3>' +  (props ?
+        '<b>' + props.nameCN + '</b>'
+        : '<i>Hover over a province</i>');
+};
+
+info.addTo(map);
