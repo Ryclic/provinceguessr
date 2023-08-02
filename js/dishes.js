de008c19ed81a7b1462d62a47b28862a04ad2f2f
Ryclic <ryanchen175@gmail.com>
2023-08-01 19:44:16 -0700

    fix github pages issue

--- js/dishes.js
+++ js/dishes.js
@@ -2,7 +2,7 @@ const data = {
     "Sichuan": [
         {   
             name: "Kung Pao Chicken",
-            description: "Kung pao chicken (宫保鸡丁gōngbǎo jīdīng /gong-baoww jee-ding/ 'palace-kept chicken cubes') is a traditional Sichuan dish made with chicken, chili, Sichuan peppers, peanuts, and vegetables. The chicken tastes tender and smooth, and the flavor can be either spicy or mild according to how much chili is added.",
+            description: "Kung pao chicken is a traditional Sichuan dish made with chicken, chili, Sichuan peppers, peanuts, and vegetables. The chicken tastes tender and smooth, and the flavor can be either spicy or mild according to how much chili is added.",
             image: "Sichuan/kungpaochicken.jpg"
         },
         {
