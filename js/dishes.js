ca29a598ba2915f846832200f5cacb2b09f66e90
Ryclic <ryanchen175@gmail.com>
2023-08-02 19:53:21 -0700

    add more dishes

--- js/dishes.js
+++ js/dishes.js
@@ -17,7 +17,51 @@ const data = {
             description: "Peking duck is a dish from Beijing that has been prepared since the Imperial era. The meat is characterized by its thin, crispy skin, with authentic versions of the dish serving mostly the skin and little meat, sliced in front of the diners by the cook.",
             image: "Beijing/peking.jpg"
         },
+    ],
+    "Guangdong": [
+        {
+            name: "Har gow",
+            description: "Har gow is a traditional Cantonese dumpling served as dim sum. These shrimp dumplings are transparent and smooth. The prawn dumplings first appeared in Guangzhou outskirts near the creek bazaar Deli. The skin must be thin and translucent, yet be sturdy enough not to break when picked up with chopsticks. It must not stick to the paper, container or the other ha gow in the basket. The shrimp must be cooked well, but not overcooked.",
+            image: "Guangdong/hargow.jpg"
+        }
+    ],
+    "Anhui": [
+        {
+            name: "Fried Hairy Tofu",
+            description: "Hairy tofu is a local favorite in China's Anhui province. It has a similar texture to blue cheese, known for its long and thick hairs due to the process of fermentation. Hairy tofu is best served with chili sauce, and is prepared through oil frying.",
+            image: "Anhui/hairytofu.jpg"
+        }
+    ],
+    "Fujian": [
+        {
+            name: "Buddha Jumps Over the Wall",
+            description: "Buddha Jumps Over the Wall, also known as Buddha's Temptation or Fotiaoqiang is a variety of shark fin soup in Fujian cuisine. The dish's name is an allusion to the dish's ability to entice the vegetarian Buddhist monks from their temples to partake in the meat-based dish, and implies that even the strictly vegetarian Gautama Buddha would try to jump over a wall to sample it.[4] It is high in protein and calcium.",
+            image: "Fujian/buddha.jpg"
+        },
+        {
+            name: "Lychee pork",
+            description: "Lychee pork or lizhi rou is a well known dish in Fujianese cuisine. It consists of small pieces of deep-fried pork and sliced water chestnuts served in a sweet and sour sauce. Its bright red colour is derived from red yeast rice. The sauce is now commonly made with ketchup, soy sauce and rice vinegar.",
+            image: "Fujian/lycheepork.jpg"
+        },
+        {
+            name: "Pepper bun",
+            description: "Hújiāo bǐng or pepper bun is a type of baked bun that originated in city of Fuzhou, the capital of China's Fujian province. It is a street food that has become popular in Taiwan and can be found in night markets or mini food stalls throughout Taiwan. The common ingredients are flour, water, and a leavening agent for the outer dough shell, and a meat protein (usually pork or beef) marinated with sugar, soy sauce, white pepper or black pepper, and scallion for the inside filling.",
+            image: "Fujian/pepperbun.jpg"
+        },
+        {
+            name: "Oyster omelette",
+            description: "Oyster omelet originated in the city of Chaozhou (Teochew) and the region of Fujian in China. It is a signature dish of the Hokkien people - Chinese diaspora in Taiwan, Malaysia, Singapore, Indonesia, and the Philippines. The dish consists of small oysters added to a mixture of potato starch and egg batter.",
+            image: "Fujian/oyster.jpg"
+        }
+    ],
+    "Yunnan": [
+        {
+            name: "Crossing-the-bridge noodles",
+            description: "Crossing-the-bridge noodles is a rice noodle soup originating from the Yunnan province of China. These noodles have over a century of history and are listed as an intangible cultural heritage of Kunming city in 2008 to promote Yunnan food culture. The dish is served with a large bowl of boiling hot broth and soup. The soup is made with chicken, pork bone and seasoning, such as Chinese star anise and ginger.",
+            image: "crossingbridge.jpg"
+        }
     ]
+
 }
 
 export {data};
